6ac478c29a6298d383dd696342c101b74503ab2a
Sujay Jayakar <sujayakar@dropbox.com>
2017-07-25 13:03:21 -0700

    add rust-cpython

---
+++ rust-cpython/python3-sys/src/intrcheck.rs
@@ -0,0 +1,8 @@
+use libc::c_int;
+
+#[cfg_attr(windows, link(name="pythonXY"))] extern "C" {
+    pub fn PyOS_InterruptOccurred() -> c_int;
+    pub fn PyOS_InitInterrupts() -> ();
+    pub fn PyOS_AfterFork() -> ();
+}
+
